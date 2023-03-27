8edb01da0d19c96f3323d3b8464cb7386b8dd931
Meenu Katariya <102709273+MeenuKatariya@users.noreply.github.com>
2023-03-27 22:18:58 +0530

    navbar-work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar } from './components/Navbar';
+import { Navbar } from './components/Navbar';
 function App() {
   return (
     <div className="App">
-    <NavBar/>
+    <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import "../css/Navbar.css";
+import Button from '@mui/material/Button';
+import logo from "../Gallery/mb.png"
 export const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='container'>
+      <div className='logo'>
+        <img src="./logo" alt="" />
+      </div>
+      <div className='menu'>
+        <div>Our Story</div>
+        <div>Write</div>
+        <div>Sign In</div>
+        <Button variant="contained">Contained</Button>
+      </div>
+      <div className='name-person'>NAME-LOGO</div>
+      </div>
   )
 }
